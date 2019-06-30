340a3869ba94a1e0a82c009890dfb41135ca5be6
Stephen-PC\Stephen <nostalgicbear@hotmail.com>
2019-06-30 19:48:46 +0100

    Adding basic nav mesh example

---
+++ Assets/Scripts/RandomRotation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Quaternion randomRotation = Random.rotation;
+
+
+        transform.rotation = new Quaternion(transform.rotation.x, randomRotation.y, transform.rotation.z, transform.rotation.w);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
